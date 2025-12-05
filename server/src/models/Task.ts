7497dd31e8d40cc5319779ecf24c4699fc4d4eac
tshusima-123 <adityakanodia2006@gmail.com>
2025-12-05 18:29:28 +0530

    Task schema created and obj added

--- server/src/models/Task.ts
+++ server/src/models/Task.ts
@@ -14,3 +14,21 @@ export interface ITask extends Document {
   modNotes?: string;// notes for a task given by either admin or the mentor
   createdAt: Date;
 }
+
+const TaskSchema = new Schema<ITask>({
+  title: { type: String,
+     required: true },
+  description: String,
+  difficulty: { type: String, 
+    enum: ["EASY","MEDIUM","HARD"], 
+    default: "MEDIUM" },
+  techStack: [String],
+  expectedTeamSize: Number,
+  deadline: Date,
+  createdBy: { type: Schema.Types.ObjectId },
+  status: { type: String, enum: ["PENDING","ACTIVE","APPROVED","REJECTED","REMOVED"], default: "PENDING" },
+  modNotes: String,
+  createdAt: { type: Date, default: Date.now }
+});
+
+export const Task = mongoose.model<ITask>("Task", TaskSchema);
